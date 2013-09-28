de39bb4a8fcab7c7f3a392adc69e1204faa67b10
Natasha Romanoff <black.widow@techie.com>
2013-09-28 21:32:45 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scrumptious::Application.config.secret_token = '9774f68320f145431b6dbc0d4a2fe2559de152865f3e7c2301ea080fc44735811a0aea26408dd4e0f9363a88b76b20ea06ecdb4b155cbd07f2c205d7ae01aef6'
