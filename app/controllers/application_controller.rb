987def482e72672ae8c5e1af99d2e945c5c3e8f4
Natasha Romanoff <black.widow@techie.com>
2013-09-29 11:43:13 +0530

    styles added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate_user!
+  
+  layout :choose_layout
+
+
+  protected
+  
+  def choose_layout
+  	if devise_controller?
+  		"devise"
+  	else
+  		"application"
+  	end
+  end
+
 end
