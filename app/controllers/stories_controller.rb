cdbd1bfa61741e64c6efbbdeb4f0e05c7273b4cf
Natasha Romanoff <black.widow@techie.com>
2013-10-02 00:42:28 +0530

    stories show and points display

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -4,4 +4,10 @@ class StoriesController < ApplicationController
 		@story = @project.backlog.stories.create(params[:story])
 		redirect_to project_backlog_path(@project)
 	end
+
+	def show
+		@project = Project.find(params[:project_id])
+		@story = @project.backlog.stories.find(params[:id])
+		
+	end
 end
